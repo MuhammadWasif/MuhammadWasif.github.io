8ef5758d182bba72f6dd97ff11709039c212eee4
MuhammadWasif <wasif33@outlook.com>
2021-04-25 14:22:24 +0500

    new project and scroll to top button

---
+++ precache-manifest.4294b9236be5d6ff233605e626f5fafe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb697e1f23325afbff1e9279f4dacb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb688fc6d337216a204",
+    "url": "/static/css/2.f01c5455.chunk.css"
+  },
+  {
+    "revision": "545c2c0aaeb6622a1c5b",
+    "url": "/static/css/main.f48d1033.chunk.css"
+  },
+  {
+    "revision": "6cb688fc6d337216a204",
+    "url": "/static/js/2.6e8e10bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e8e10bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545c2c0aaeb6622a1c5b",
+    "url": "/static/js/main.00754407.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codein-logo.47a7a717.webp"
+  },
+  {
+    "revision": "2836f5eb0cfef31d89efbc8ea764d1d2",
+    "url": "/static/media/profile_pic.2836f5eb.jpg"
+  }
+]);
